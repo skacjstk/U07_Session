f9bd5dbe19f0cf90c8e38038122ec96826d46961
seok won nam <skacjstk0818@gmail.com>
2023-02-09 16:50:42 +0900

    [2023.02.09] What is Session
    강의 위주로 진행해서 코드가 없다시피함.
    Cmd로 IP 넣어서 창띄워본것 외에 이론 다수

---
+++ Source/Game/CMovingPlatform.cpp
@@ -0,0 +1,17 @@
+#include "CMovingPlatform.h"
+
+ACMovingPlatform::ACMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+
+}
+void ACMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector location = GetActorLocation();
+	location += FVector(Speed * DeltaTime, 0, 0);
+
+	SetActorLocation(location);
+}
